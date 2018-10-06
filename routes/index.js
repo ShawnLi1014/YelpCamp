5b2f7207a9202f7dc3e87e113cb13635333ddc32
ShawnLi1014 <shawnli1014@gmail.com>
2018-10-06 14:44:33 +0800

    bootstrap4 adjustment

--- routes/index.js
+++ routes/index.js
@@ -48,7 +48,9 @@ router.get("/login", function(req, res) {
 router.post("/login", passport.authenticate("local", 
     {
         successRedirect:"/campgrounds",
-        failureRedirect:"/login"
+        failureRedirect:"/login",
+        failureFlash: true,
+        successFlash: 'Welcome to YelpCamp!'
     }),function(req, res){
 });
 
